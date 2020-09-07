2aebdf7da9ed6c067c15832704163bf53e46ce9b
xdgerardxd <gerardomorillo10@gmail.com>
2020-09-07 18:56:59 -0300

    db env

--- src/keys.js
+++ src/keys.js
@@ -7,9 +7,9 @@ console.log("Importando config DB en keys.js");
 module.exports = {
     //Objeto de configuracion de mi base de datos
     database: {
-        host: 'localhost', // Donde esta alojada
-        user: 'root', // Datos de conexion
-        password: 'Sharingan321',
-        database: 'database_links' // nombre DB
+        host: process.env.DB_HOST, // Donde esta alojada localhost
+        user: process.env.DB_USER, // Datos de conexion root
+        password: process.env.DB_PASS, //Sharingan321
+        database: process.env.DB_DATABASE // database_ links
     }
 };
